1154feb3a8fc9577ee372da807e2b01dd2342d80
Artem <avd.qu6@gmail.com>
2020-07-07 16:37:20 +0500

    Продолжаю главную

--- src/js/common.js
+++ src/js/common.js
@@ -1,8 +1,33 @@
 $(document).ready(function() {
 
+    // Поиск в хедере
     $('.js--header-search-button').click( function(event) {
         event.preventDefault();
         $(this).closest('.js--header-search').toggleClass('active');
     });
 
+    // Слайдер Врачей на главной
+    $('.js--main-slider').slick({
+        infinite: false,
+        slidesToShow: 1,
+        arrows: true,
+        dots: false,
+        slidesToScroll: 1,
+        prevArrow: $('.js--main-slider__prev'),
+        nextArrow: $('.js--main-slider__next'),
+        responsive: [
+            {
+                breakpoint: 993,
+                settings: {
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 769,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+        ]
+    });
 });
